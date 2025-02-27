14617e3e2b6280a23b5ca7e2cacb96740145690b
Joshua Hew <66423860+J2hwz@users.noreply.github.com>
2025-02-27 22:46:42 +0000

    Update darkmode functionalities

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
-// // Set dark mode
-// function setDarkMode(dark, preference) {
-//   if (dark) { // Set dark mode 
-//     // Set as the theme if user overriding OS preference (remove theme setting if respecting OS preference)
-//     preference !== "dark" ? localStorage.setItem('theme', 'dark') : localStorage.removeItem('theme');
-//     // Change to dark mode by adding class to <html> element 
-//     document.documentElement.classList.add('dark');
-//   }  else if (!dark) { // Remove dark mode 
-//     // Set as the theme if user overriding OS preference (remove theme setting if respecting OS preference)
-//     preference !== "light" ? localStorage.setItem('theme', 'light') : localStorage.removeItem('theme');
-//     // Change to light mode by removing class from <html> element
-//     document.documentElement.classList.remove('dark');
-//   }
-// }
-
-// // Check if we need to add dark class from theme or OS preference
-// const preference = window.matchMedia('(prefers-color-scheme: dark)'). matches ? 'dark' : 'light';
-// if (localStorage.getItem('theme') === "dark" || (!('theme' in localStorage) && preference === 'dark')) {
-//   setDarkMode(true, preference);
-// }
-
-// function toggleDarkMode() {
-//   document.body.classList.toggle('dark-mode');
-// }
-
+var darkmode;
 
 // Remember user's setting 
 function toggleDarkMode() {
-  let isDark = document.body.classList.toggle('dark-mode');
-  localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+    let isDark = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+
+    document.querySelector(".dark-mode-button").textContent = isDark ? "lighter" : "darker";
 }
 
 // On page load
 document.addEventListener('DOMContentLoaded', (event) => {
-  if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-  }
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+        document.querySelector(".dark-mode-button").textContent = "lighter";
+    } else {
+        document.querySelector(".dark-mode-button").textContent = "darker";
+    }
+});
+
+// Add event listener to the link
+document.querySelector('.dark-mode-butto').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    toggleDarkMode();
 });
